be5d635cb168f09a28042fc1efbb488e1f363e49
Josh Kilgore <joshkilgore@Joshs-MacBook-Pro.local>
2016-04-05 20:30:18 -0500

    Added objects to private array

--- CarLot.js
+++ CarLot.js
@@ -11,36 +11,36 @@ var CarLot = (function () {
       //add event listener for "load" and set anonymous callback function
       loader.addEventListener("load", function () {
       privateInventory = JSON.parse(this.responseText).cars;
+      console.log("privateInventory", privateInventory);
       });
       //set value to private array
-console.log("privateInventory", privateInventory);
-        }
-    },
+         },
+    //set public getter
     getInventory: function() {
         return privateInventory; 
     }
  }
-}();
+}());
+
+
+function listCars(privateInventory) {
+  var list = document.getElementsByClassName("container");
+  var outputString = "";
+
+  for (var i = 0; i < privateInventory.length; i++) {
+    var currentCar = privateInventory[i];
+
+    //build DOM string
+    outputString += `<h1>${currentCar.make}</h1>`;
+    outputString += `<h3>${currentCar.model}</h3>`;
+    outputString += `<h5>${currentCar.year}</h5>`;
+    outputString += `<h5>${currentCar.price}</h5>`;
+    outputString += `<h5>${currentCar.color}</h5>`;
+    outputString += `<h5>${currentCar.purchased}</h5>`;
+    outputString += `<h5>${currentCar.description}</h5>`;
+  }
+  
+ list.innerHTML = outputString;
+}
 
 CarLot.loadCars();
-
-// function listCars(inventory) {
-//   var list = document.getElementsByClassName("container");
-//   var outputString = "";
-
-//   for (var i = 0; i < inventory.length; i++) {
-//     var currentCar = inventory[i];
-
-//     //build DOM string
-//     outputString += `<h1>${currentCar.make}</h1>`;
-//     outputString += `<h3>${currentCar.model}</h3>`;
-//     outputString += `<h5>${currentCar.year}</h5>`;
-//     outputString += `<h5>${currentCar.price}</h5>`;
-//     outputString += `<h5>${currentCar.color}</h5>`;
-//     outputString += `<h5>${currentCar.purchased}</h5>`;
-//     outputString += `<h5>${currentCar.description}</h5>`;
-//   }
-//   
-//  list.innerHTML = outputString;
-// }
-
